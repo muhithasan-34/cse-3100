921d9bd6b2bbfc89ef561d47380f991f6c163843
Muhit Hasan Chowdhury <muhithasanlol3234@gmail.com>
2024-12-21 21:38:09 +0600

    updated about and contact page

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
-export default function AboutUs() {
+const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div>
+      <h1>About Us</h1>
+      <p>
+      Welcome to our website for cat adoption!
+      Our mission is to link pet owners with their ideal feline companions and assist cats in finding loving homes.
+
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
